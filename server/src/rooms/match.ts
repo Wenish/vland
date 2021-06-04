8a1c598d0a0584142caa940262bb98acb630dbe6
Wenish <Wenish@users.noreply.github.com>
2021-06-04 21:14:56 +0200

    Update match.ts

--- server/src/rooms/match.ts
+++ server/src/rooms/match.ts
@@ -4,18 +4,21 @@ import { Room, Client } from "colyseus";
 export class RoomMatch extends Room {
     // When room is initialized
     onCreate (options: any) {
-        console.log(`[GameRoom] onCreate`)
+        console.log(`[GameRoom] onCreate ${this.roomName} ${this.roomId}`)
+        this.onMessage("*", (client, type, message) => {
+            console.log(`[GameRoom] onMessage Client: ${client.sessionId} sent ${type} ${JSON.stringify(message)}`)
+        });
     }
 
     // Authorize client based on provided options before WebSocket handshake is complete
     onAuth (client: Client, options: any, request: http.IncomingMessage) {
-        console.log(`[GameRoom] onAuth Client with sessionId: ${client.sessionId}`)
+        console.log(`[GameRoom] onAuth Client: ${client.sessionId}`)
         return true;
     }
 
     // When client successfully join the room
     onJoin (client: Client, options: any, auth: any) {
-        console.log(`[GameRoom] onJoin Client with sessionId: ${client.sessionId}`)
+        console.log(`[GameRoom] onJoin Client: ${client.sessionId}`)
     }
 
     // When a client leaves the room
@@ -25,6 +28,6 @@ export class RoomMatch extends Room {
 
     // Cleanup callback, called after there are no more clients in the room. (see `autoDispose`)
     onDispose () {
-        console.log(`[GameRoom] onDispose`)
+        console.log(`[GameRoom] onDispose ${this.roomName} ${this.roomId}`)
     }
 }
