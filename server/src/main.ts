95bbe02052e901c3b8474d1ad3b24ab5068774f1
Wenish <Wenish@users.noreply.github.com>
2021-06-04 20:48:29 +0200

    feat: adding match room

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,9 @@
 import { Server } from "colyseus"
+import { RoomMatch } from "./rooms/match";
 const port = parseInt(process.env.port, 10) || 3000
 
 const gameServer = new Server()
+// Define "match" room
+gameServer.define("match", RoomMatch);
 gameServer.listen(port)
 console.log(`[GameServer] Listening on Port: ${port}`)
