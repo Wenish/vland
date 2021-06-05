f835f527efe250aecd0cd960e94b49c1bf85bd7a
Wenish <Wenish@users.noreply.github.com>
2021-06-05 04:27:21 +0200

    update state

--- server/src/match/match.room.ts
+++ server/src/match/match.room.ts
@@ -1,11 +1,22 @@
 import http from "http";
 import { Room, Client } from "colyseus";
 import logger from "../services/logger.service";
-import { MatchState } from "./match.state";
+import { CapturePointState, MatchState, PlayerState, PositionState } from "./match.state";
 
 export class MatchRoom extends Room<MatchState> {
     onCreate (options: any) {
-        this.setState(new MatchState())
+        const state = new MatchState()
+        // TODO: load map json
+        state.map.capturePoints.set('1', new CapturePointState().assign({
+            id: '1',
+            position: new PositionState().assign({
+                x: 10,
+                y: 20,
+                z: 0
+            }),
+            radius: 15
+        }))
+        this.setState(state)
         logger(`onCreate ${this.roomName} ${this.roomId}`, 'GameRoom')
         this.onMessage("*", (client, type, message) => {
             logger(`onMessage Client: ${client.sessionId} sent ${type} ${JSON.stringify(message)}`, 'GameRoom')
@@ -18,10 +29,15 @@ export class MatchRoom extends Room<MatchState> {
     }
 
     onJoin (client: Client, options: any, auth: any) {
+        this.state.players.set(client.sessionId, new PlayerState().assign({
+            sessionId: client.sessionId,
+            username: `Player ${client.sessionId}`
+        }))
         logger(`onJoin Client: ${client.sessionId}`, 'GameRoom')
     }
 
     onLeave (client: Client, consented: boolean) {
+        this.state.players.delete(client.sessionId)
         logger(`onLeave Client with sessionId: ${client.sessionId}`, 'GameRoom')
     }
 
--- server/src/match/match.state.ts
+++ server/src/match/match.state.ts
@@ -1,15 +1,47 @@
-import { Schema, type } from "@colyseus/schema"
+import { MapSchema, Schema, type } from "@colyseus/schema"
 
-class MapSizeState extends Schema {
+export class PositionState extends Schema {
+    @type('number') x: number = 0
+    @type('number') y: number = 0
+    @type('number') z: number = 0
+}
+
+export class CapturePointState extends Schema {
+    @type('string') id: string
+    @type(PositionState) position: PositionState
+    @type('number') radius: number
+}
+
+export class MapSizeState extends Schema {
     @type('number') width: number = 100
     @type('number') height: number = 200
 }
 
-class MapState extends Schema {
+export class MapState extends Schema {
     @type('string') mapName: string = 'cs_default'
-    @type(MapSizeState) mapSizeState: MapSizeState = new MapSizeState()
+    @type(MapSizeState) mapSize: MapSizeState = new MapSizeState()
+    @type({ map: CapturePointState }) capturePoints = new MapSchema<CapturePointState>()
+}
+
+export class TeamState extends Schema {
+    @type('string') id: string
+    @type('string') color: string
+    @type('number') score: number = 0
+}
+
+export class PlayerState extends Schema {
+    @type('string') public sessionId: string
+    @type('string') public username: string = 'Player'
+}
+
+export class UnitState extends Schema {
+    @type('string') public id: string
+    @type('string') public name: string = 'Unit Name'
 }
 
 export class MatchState extends Schema {
-    @type(MapState) mapName: MapState = new MapState()
+    @type(MapState) map: MapState = new MapState()
+    @type({ map: TeamState }) teams = new MapSchema<TeamState>()
+    @type({ map: PlayerState }) players = new MapSchema<PlayerState>()
+    @type({ map: UnitState }) units = new MapSchema<UnitState>()
 }
