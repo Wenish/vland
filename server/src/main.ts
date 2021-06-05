32857fc481cc4172cce2e2d205a5e0253de0cf13
Wenish <Wenish@users.noreply.github.com>
2021-06-05 02:29:24 +0200

    adding colyseus monitor and express

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,16 @@
 import { Server } from "colyseus"
+import { createServer } from "http";
+import express from 'express'
+import { monitor } from "@colyseus/monitor";
 import { RoomMatch } from "./rooms/match";
 import logger from "./services/logger";
 
 const port = parseInt(process.env.port, 10) || 3000
-const gameServer = new Server()
-// Define "match" room
-gameServer.define("match", RoomMatch);
+const app = express()
+app.use('/colyseus', monitor());
+const gameServer = new Server({
+    server: createServer(app)
+  });
+gameServer.define('match', RoomMatch);
 gameServer.listen(port)
 logger(`Listening on Port: ${port}`, 'GameServer')
