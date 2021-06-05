cbf700f325ac3022006e65f9ff17576f7d5fbcf2
Wenish <Wenish@users.noreply.github.com>
2021-06-05 05:57:42 +0200

    Update logger.service.ts

--- server/src/services/logger.service.ts
+++ server/src/services/logger.service.ts
@@ -1,6 +1,6 @@
 const logger = (text: string, context?: string) => {
     const timestamp = new Date().toTimeString().substring(0, 8)
-    console.log(`[\x1b[33m${context || 'Server'}\x1b[37m] \x1b[90m${timestamp} \x1b[37m${text}`,)
+    console.log(`[\x1b[33m${context || 'Server'}\x1b[37m] \x1b[90m${timestamp} \x1b[37m${text}`)
 }
 
 export default logger
