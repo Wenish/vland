b7b80577ec8f85bf634074a87e11af44126a60ac
Wenish <Wenish@users.noreply.github.com>
2021-06-05 05:55:09 +0200

    Update logger.service.ts

--- server/src/services/logger.service.ts
+++ server/src/services/logger.service.ts
@@ -1,6 +1,5 @@
 const logger = (text: string, context?: string) => {
-    const currentDate = new Date()
-    const timestamp = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`
+    const timestamp = new Date().toTimeString().substring(0, 8)
     console.log(`[\x1b[33m${context || 'Server'}\x1b[37m] \x1b[90m${timestamp} \x1b[37m${text}`,)
 }
 
