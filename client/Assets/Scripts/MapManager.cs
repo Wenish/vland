c5818f29994de9877d2fa8d01835d98b5213464a
Wenish <Wenish@users.noreply.github.com>
2021-06-12 05:03:55 +0200

    adding MapManager

---
+++ client/Assets/Scripts/MapManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    private static MapManager _instance;
+
+    public static MapManager Instance
+    {
+        get { return _instance; }
+    }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
